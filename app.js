9fd7a937888b18a949e7f2457014c5095482ea7d
Alex Morega <alex@grep.ro>
2014-11-01 16:48:16 +0200

    remove latin-2 characters

--- app.js
+++ app.js
@@ -13,7 +13,24 @@
 
         var doc = new jsPDF();
 
-        doc.fromHTML($('#document').get(0), 15, 15, {
+        var html = $('#document').html();
+        var charmap = [
+            ['ș', 's'],
+            ['ț', 't'],
+            ['ă', 'a'],
+            ['â', 'a'],
+            ['î', 'i'],
+            ['Ș', 'S'],
+            ['Ț', 'T'],
+            ['Ă', 'A'],
+            ['Â', 'A'],
+            ['Î', 'I']
+        ];
+        charmap.forEach(function(pair) {
+            html = html.replace(new RegExp(pair[0], 'g'), pair[1]);
+        });
+
+        doc.fromHTML(html, 15, 15, {
             'width': 170,
             'elementHandlers': {
                 '#editor': function (element, renderer) {
